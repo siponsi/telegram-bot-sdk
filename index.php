20996797a621b26fd05c770e523f391f4f3492d9
Oscars12 <46792229+Oscars12@users.noreply.github.com>
2020-06-14 15:17:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,15 +6,15 @@ $telegram = new Api('1231883463:AAFjcudIcpcB3MIvyYAodAsMpG_yf5JK4cs');
 
 // $response = $telegram->getMe();
 
-// $botId = $response->getId();
-// $firstName = $response->getFirstName();
-// $username = $response->getUsername();
-// echo "$botId $firstName $username";
+$botId = $response->getId();
+$firstName = $response->getFirstName();
+$username = $response->getUsername();
+echo "$botId $firstName $username";
 // $telegram
 //    ->setAsyncRequest(true)
 //    ->sendPhoto(['chat_id' => 'CHAT_ID', 'photo' => 'path/to/photo.jpg']);
 
 // $response = $telegram->setWebhook(['url' => 'https://tbotsdk.herokuapp.com/1231883463:AAFjcudIcpcB3MIvyYAodAsMpG_yf5JK4cs/webhook']);
 // $updates = $telegram->getWebhookUpdates();
-$messageId = $response->getMessageId();
-var_dump($messageId);
+// $messageId = $response->getMessageId();
+// var_dump($messageId);

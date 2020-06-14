688421b45343a520ab9890b95e8744cb3fe66196
Oscars12 <46792229+Oscars12@users.noreply.github.com>
2020-06-14 13:24:14 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,15 @@ use Telegram\Bot\Api;
 
 $telegram = new Api('1231883463:AAFjcudIcpcB3MIvyYAodAsMpG_yf5JK4cs');
 
-$response = $telegram->getMe();
+// $response = $telegram->getMe();
 
-$botId = $response->getId();
-$firstName = $response->getFirstName();
-$username = $response->getUsername();
-echo "$botId $firstName $username";
+// $botId = $response->getId();
+// $firstName = $response->getFirstName();
+// $username = $response->getUsername();
+// echo "$botId $firstName $username";
 // $telegram
 //    ->setAsyncRequest(true)
 //    ->sendPhoto(['chat_id' => 'CHAT_ID', 'photo' => 'path/to/photo.jpg']);
 
 $updates = $telegram->getWebhookUpdates();
-print_r($updates);
+var_dump($updates);

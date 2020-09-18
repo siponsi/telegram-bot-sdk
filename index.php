acbbeaa715bdbabaedfd41925555068199501c06
Oscars12 <46792229+Oscars12@users.noreply.github.com>
2020-09-18 12:08:47 -0400

    update to send only

--- index.php
+++ index.php
@@ -4,21 +4,27 @@ require 'vendor/autoload.php';
 use Telegram\Bot\Api;
 // Esto es un comentario ok
 $telegram = new Api('1231883463:AAFjcudIcpcB3MIvyYAodAsMpG_yf5JK4cs', true);
+$id= 843856890;
+//
+//$updates = $telegram->getWebhookUpdates();
+//$message = $updates->getMessage();
+//
+//$chatId = $message->getChat()->getId();
+//$enviedText = $message->getText();
 
-$updates = $telegram->getWebhookUpdates();
-$message = $updates->getMessage();
 
-$chatId = $message->getChat()->getId();
-$enviedText = $message->getText();
-
-if (isset($updates) & $updates <> null & $message <> null) {
-    $response = $telegram->sendMessage([
-        'chat_id' => "$chatId",
-        'text' => "Gracias por escribirnos"
-    ]);
-    $response = $telegram->sendPhoto([
-        'chat_id' => "$chatId",
-        'photo' => 'https://siponsi.github.io/a.JPG',
-        'caption' => 'Some caption'
-    ]);
-}
+//if (isset($updates) & $updates <> null & $message <> null) {
+//    $response = $telegram->sendMessage([
+//        'chat_id' => "$chatId",
+//        'text' => "Gracias por escribirnos"
+//    ]);
+//    $response = $telegram->sendPhoto([
+//        'chat_id' => "$chatId",
+//        'photo' => 'https://siponsi.github.io/a.JPG',
+//        'caption' => 'Some caption'
+//    ]);
+//}
+$response = $telegram->sendMessage([
+    'chat_id' => "$id",
+    'text' => "Gracias por escribirnos"
+]);

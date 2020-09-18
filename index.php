2f6408b18a6ed2d0501a90875f87c4f965b0a707
Oscars12 <46792229+Oscars12@users.noreply.github.com>
2020-09-18 11:37:05 -0400

    index

--- index.php
+++ index.php
@@ -1,43 +1,24 @@
 <?php
 require 'vendor/autoload.php';
-use Telegram\Bot\Api;
 
+use Telegram\Bot\Api;
+// Esto es un comentario ok
 $telegram = new Api('1231883463:AAFjcudIcpcB3MIvyYAodAsMpG_yf5JK4cs', true);
 
-// $response = $telegram->getMe();
-
-// $botId = $response->getId();
-// $firstName = $response->getFirstName();
-// $username = $response->getUsername();
-// echo "$botId $firstName $username";
 $updates = $telegram->getWebhookUpdates();
-$m=$updates->getMessage();
-$ui=$updates->getUpdateId();
-$t=$m->getText();
-$c=$m->getChat();
-$ci=$c->getId();
-if (isset($ui) & $ui <> null){
+$message = $updates->getMessage();
+
+$chatId = $message->getChat()->getId();
+$enviedText = $message->getText();
+
+if (isset($updates) & $updates <> null & $message <> null) {
     $response = $telegram->sendMessage([
-        'chat_id' => "$ci",
-        'text' => "the stupid ot wrote this (el estudido de oti escribio esto) $t desde $ci"
+        'chat_id' => "$chatId",
+        'text' => "Gracias por escribirnos"
     ]);
     $response = $telegram->sendPhoto([
-        'chat_id' => "$ci",
+        'chat_id' => "$chatId",
         'photo' => 'https://siponsi.github.io/a.JPG',
-        'caption' => 'Some caption cambio iPhone'
+        'caption' => 'Some caption'
     ]);
-
-
 }
-// $message= $updates->getMessage();
-
-// $messageId = $response->getMessageId();
-// $telegram
-//    ->setAsyncRequest(true)
-//    ->sendPhoto(['chat_id' => 'CHAT_ID', 'photo' => 'path/to/photo.jpg']);
-
-// $response = $telegram->setWebhook(['url' => 'https://tbotsdk.herokuapp.com/1231883463:AAFjcudIcpcB3MIvyYAodAsMpG_yf5JK4cs/webhook']);
-// $updates = $telegram->getWebhookUpdates();
-// $messageId = $response->getMessageId();
-// var_dump($messageId);
-// probando desde el iPhone 
